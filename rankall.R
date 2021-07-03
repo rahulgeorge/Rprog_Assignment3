23d75fb526d25162590a6d6f1359cc0654f57965
Rahul George <rahulgeorge.secure@gmail.com>
2021-07-04 00:47:36 +0530

    Completed all assignments

--- rankall.R
+++ rankall.R
@@ -1,9 +1,9 @@
 #Final Assignment 3 July
 
-rankhospital <- function(outcome = character(), num) {
+rankall <- function(outcome = character(), num = "best") {
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character") #Reading in the data
-        data[,7] <- as.factor(data[,7])
-        state_lvl <- levels(as.factor(data[,7]))
+        #data[,7] <- as.factor(data[,7])
+        #state_lvl <- levels(as.factor(data[,7]))
         # if(!(state %in% state_lvl)) return(message("State not recognised"))
         # stateData <- data[data$State == state,] #Filtering out data for just the state specified
         
@@ -12,17 +12,33 @@ rankhospital <- function(outcome = character(), num) {
         else if(outcome == "heart failure") t <- 17
         else if(outcome == "pneumonia") t <- 23
         else return(message("Outcome not recognised"))
-        mor <- stateData[,c(2,t)] #Reducing data down to just the outcome specified and hospital name
+        allHosp <- data[,c(2,7,t)] #Reducing data down to just the outcome specified and hospital name
         
-        mor[[2]] <- as.numeric(mor[[2]])
-        good <- complete.cases(mor[[2]])
-        cleanMor <- mor[good,] #Clean data generated. NA values removed
-        cleanMor <- cleanMor[order(cleanMor[,2],cleanMor[,1]),] #Ordering by ascending values of mortality rate & hospital name
-        
-        if(num == "best") num <- 1      #Handling values for num
-        else if(num == "worst") num <- length(cleanMor$Hospital.Name) 
-        else if(num > length(cleanMor$Hospital.Name)) return(NA)
+        allHosp[[3]] <- as.numeric(allHosp[[3]])
+        good <- complete.cases(allHosp[[3]])
+        cleanHosp <- allHosp[good,] #Clean data generated. NA values removed
+        cleanHosp <- cleanHosp[order(cleanHosp[,2],cleanHosp[,3],cleanHosp[,1]),] #Sorts all values by the state, rate and then hosp name
        
-        hosp <- as.character(cleanMor[num,][1])
-        hosp
+        if(num == "best") num <- 1 #Handling values for num
+        else if(num != "worst" && num > length(cleanHosp$Hospital.Name)) return(NA)
+        
+        statesComplete <- character() #Expecting to receive all states evaluated
+        rankedHosp <- data.frame(hospital = character(), state = character()) #Preparing to receive the final output
+        runningRank <- 1 #keeping track of required rank
+        for (i in 1:length(cleanHosp$Hospital.Name)) { #For all hospital records
+                state <- as.character(cleanHosp$State[i]) #current state being worked on
+                if(is.na(statesComplete[1])) statesComplete <- append(statesComplete, state) #Checking for first run of loop
+                runningState <- statesComplete[length(statesComplete)] #State in previous run checked to handle rank
+                
+                if(state == runningState && i > 1 && i != length(cleanHosp$Hospital.Name)) runningRank <- runningRank + 1 #Initial run setup as well as necessary conditions to offset further ifelse
+                else if(state != runningState) { #Resetting runningRank when state changes as well as worst rank handling when state changes
+                        runningRank <- 1
+                        statesComplete[length(statesComplete)+1] <- state #Capturing the completed state
+                        if(num == "worst") rankedHosp[length(statesComplete),] <- cleanHosp[i-1,1:2] #Handling case for worst of a state
+                }
+                else if(i == length(cleanHosp$Hospital.Name) && num == "worst") rankedHosp[length(statesComplete)+1,] <- cleanHosp[i,1:2] #Special condition at the end of the loop to handle worst
+                
+                if(runningRank == num) rankedHosp[length(statesComplete),] <- cleanHosp[i,1:2] #Capturing hosp of required rank
+        }
+        rankedHosp
 }
